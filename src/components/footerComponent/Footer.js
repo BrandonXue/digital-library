824e1069ea0ef26fc90b2c401224873c92ef4197
BrandonXue <57818806+BrandonXue@users.noreply.github.com>
2020-03-26 21:38:56 -0700

    Add files via upload
    Created basic Header and Footer components. Adopted color scheme from https://www.awwwards.com/sites/waaark. Designed logo.

---
+++ src/components/footerComponent/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="App-footer">
+            Created by Ernesto Hooghkirk and Brandon Xue
+            </footer>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/headerComponent/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+import logo from './logo.svg'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <h1 className="App-header-text">Digital Library</h1>
+                <img className="App-logo" src={logo}/>
+            </header>
+        );
+    }
+}
+
+export default Header;
