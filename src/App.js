dfd417d9c0477f34616e543317c22fde80ed8ff4
BrandonXue <brandon_xue@hotmail.com>
2020-05-23 21:26:50 -0700

    dog

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends React.Component {
       },
     ];
 
-    return ( // Ernesto
+    return ( // Dog
       <div className="App">
         <Header className='Header'/>
           <Collage className='Mosaic' media={media} music={musicList} written={writtenList}/>
