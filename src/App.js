5d9dfc742bb5891ab53ebedc043a0eb7a8838d5d
Ernie <hoogeek101@yahoo.com>
2020-05-23 21:26:17 -0700

    CATS!

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends React.Component {
       },
     ];
 
-    return ( // Ernesto
+    return ( // Cat
       <div className="App">
         <Header className='Header'/>
           <Collage className='Mosaic' media={media} music={musicList} written={writtenList}/>
