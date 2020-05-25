6ca90569dc8f7de9ec452775baefa86cc153c67c
BrandonXue <brandon_xue@hotmail.com>
2020-05-25 15:58:40 -0700

    Refactored collections

--- src/components/collectionsComponent/Collections.js
+++ src/components/collectionsComponent/Collections.js
@@ -6,31 +6,19 @@ class Collections extends React.Component {
     render() {
         return(
             <div className = "collections">  
-                <div className="collectionsColumn"  style ={{width: '30%' }}>
-                    <CollectionsItem bc = '#464866' tc = 'white' h = '22vw' j = 'space-evenly' 
-                    pt = '3vw' pr = '' pl = '' pb = '' mt = '' mr = '' ml = '' mb = '5vw'/>
-                    <CollectionsItem bc = '#AAABB8' tc = 'white' h = '16vw' j = 'space-evenly' 
-                    pt = '' pr = '' pl = '' pb = '' mt = '' mr = '' ml = '' mb = '5vw'/>
-                    <CollectionsItem bc = '#464866' tc = 'white' h = '22vw' j = 'space-evenly' 
-                    pt = '3vw' pr = '' pl = '' pb = '' mt = '' mr = '' ml = '' mb = ''/>
+                <div className="collectionsColumn">
+                    <CollectionsItem bgColor = '#464866' height = '22vw'/>
+                    <CollectionsItem bgColor = '#AAABB8' height = '16vw' marginTop = '5vw'/>
+                    <CollectionsItem bgColor = '#464866' height = '22vw' marginTop = '5vw'/>
                 </div>
-                <div className="collectionsColumn"  style ={{width: '5%' }}>
+                <div className = "collectionsColumn"> 
+                    <CollectionsItem bgColor = '#2E9CCA' height = '22vw' marginTop = '10vw'/>
+                    <CollectionsItem bgColor = '#29648A' height = '22vw' marginTop = '5vw'/>
                 </div>
-                <div className = "collectionsColumn" style ={{width: '30%'}}> 
-                    <CollectionsItem bc = '#2E9CCA' tc = 'white' h = '22vw' j = 'space-evenly' 
-                    pt = '5vw' pr = '' pl = '' pb = '' mt = '10vw' mr = '' ml = '' mb = '5vw'/>
-                    <CollectionsItem bc = '#29648A' tc = 'white' h = '22vw' j = 'space-evenly' 
-                    pt = '5vw' pr = '' pl = '' pb = '' mt = '' mr = '' ml = '' mb = ''/>
-                </div>
-                <div className="collectionsColumn"  style ={{width: '5%' }}>
-                </div>
-                <div className = "collectionsColumn" style ={{width: '30%'}}> 
-                    <CollectionsItem bc = '#AAABB8' tc = 'white' h = '22vw' j = 'space-evenly' 
-                    pt = '3vw' pr = '' pl = '' pb = '' mt = '' mr = '' ml = '' mb = '5vw'/>
-                    <CollectionsItem bc = '#464866' tc = 'white' h = '16vw' j = 'space-evenly' 
-                    pt = '' pr = '' pl = '' pb = '' mt = '' mr = '' ml = '' mb = '5vw'/>
-                    <CollectionsItem bc = '#2E9CCA' tc = 'white' h = '22vw' j = 'space-evenly' 
-                    pt = '3vw' pr = '' pl = '' pb = '' mt = '' mr = '' ml = '' mb = ''/>
+                <div className = "collectionsColumn"> 
+                    <CollectionsItem bgColor = '#AAABB8' height = '22vw'/>
+                    <CollectionsItem bgColor = '#464866' height = '16vw' marginTop = '5vw'/>
+                    <CollectionsItem bgColor = '#2E9CCA' height = '22vw' marginTop = '5vw'/>
                 </div>          
             </div> 
         );
@@ -38,6 +26,3 @@ class Collections extends React.Component {
 }
 
 export default Collections;
-
-// <div className="collectionsColumn" style={{
-//    backgroundColor: "#FFFFFF",
--- src/components/collectionsComponent/CollectionsItem.js
+++ src/components/collectionsComponent/CollectionsItem.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import './CollectionsItem.css';
 
-
 class CollectionsItem extends React.Component {
     render() {
         return(
-            <div className= "collectionsItem" style = 
-                {{backgroundColor: this.props.bc, 
-                  color: this.props.tc,
-                  width: this.props.w,
-                  height: this.props.h,
-                  justifyContent: this.props.j,
-
-                  paddingTop: this.props.pt,
-                  paddingRight: this.props.pr,
-                  paddingLeft: this.props.pl,
-                  paddingBottom: this.props.pb,
-
-                  marginTop: this.props.mt,
-                  marginRight: this.props.mr,
-                  marginLeft: this.props.ml,
-                  marginBottom: this.props.mb,}}>
+            <div className= "collectionsItem"
+                style = {{
+                    backgroundColor: this.props.bgColor, 
+                    height: this.props.height,
+                    marginTop: this.props.marginTop,
+                }}
+            >
                   oi
             </div>
         );
     }
 }
 
-export default CollectionsItem;
-
-// <div className= "collectionsColumn">  
+export default CollectionsItem;  
